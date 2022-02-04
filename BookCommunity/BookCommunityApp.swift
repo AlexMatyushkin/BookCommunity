95e4966f67fbaa2da04cb043b2a07a5cb2045023
Алексей Матюшкин <avlmatyushkin@sberbank.ru>
2022-02-04 09:03:38 +0300

    first commit

---
+++ BookCommunity/BookCommunityApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookCommunityApp.swift
+//  BookCommunity
+//
+//  Created by 19438887 on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct BookCommunityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
